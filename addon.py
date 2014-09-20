e2e2174e41154ad68a679d11aa7d61e1bd119dbf
ddelm <delmotte.david@gmail.com>
2014-09-20 23:16:20 +0200

    Update addon.py

--- addon.py
+++ addon.py
@@ -117,7 +117,7 @@ class Pilight(object):
     config = None
 
 
-    def __init__(self, host = '127.0.0.1', port = 5000):
+    def __init__(self, host = '192.168.2.18', port = 5001):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.port = port
